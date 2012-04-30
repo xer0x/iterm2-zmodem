faf584ead0187c57b6bb4e5ed3e8312d5cdedccb
Drew Miller <drew.miller@joyent.com>
2012-04-29 22:40:14 -0700

    Download to ~/Downloads & add growl

--- iterm2-recv-zmodem.sh
+++ iterm2-recv-zmodem.sh
@@ -4,17 +4,21 @@
 # licensed under cc-wiki with attribution required 
 # Remainder of script public domain
 
-FILE=`osascript -e 'tell application "iTerm" to activate' -e 'tell application "iTerm" to set thefile to choose folder with prompt "Choose a folder to place received files in"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")"`
+#FILE=`osascript -e 'tell application "iTerm" to activate' -e 'tell application "iTerm" to set thefile to choose folder with prompt "Choose a folder to place received files in"' -e "do shell script (\"echo \"&(quoted form of POSIX path of thefile as Unicode text)&\"\")"`
+FILE="/Users/"`whoami`"/Downloads"
 if [[ $FILE = "" ]]; then
 	echo Cancelled.
 	# Send ZModem cancel
 	echo -e \\x18\\x18\\x18\\x18\\x18
-	echo \# Cancelled transfer
+	echo "# Cancelled transfer       "
+	/usr/local/bin/growlnotify -a /Applications/iTerm.app -n "iTerm2.app" -m 'Cancelled.' -t "File transfer"
 	echo
 else
 	echo $FILE
-	cd "$FILE"
-	/usr/local/bin/rz 
-	echo \# Received $FILE
+	cd "${FILE}"
+	/usr/local/bin/growlnotify -a /Applications/iTerm.app -n "iTerm2.app" -m 'Transfer started...' -t "File transfer"
+	/usr/local/bin/rz
+	/usr/local/bin/growlnotify -a /Applications/iTerm.app -n "iTerm2.app" -m 'Transfer finished.' -t "File transfer"
+	echo "# Transfer finished        "
 	echo
 fi
